b239040ed02dead24f721a7c283663d4da1683b7
Ahmed J. Omer <ahmedjomer@gmail.com>
2022-01-03 20:58:50 -0800

    about section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-import Intro from '../src/components/introduction/Intro'
+import Intro from '../src/components/introduction/Intro';
+import About from '../src/components/about/About';
 const App = () => {
-  return <div><Intro/></div>;
+  return <div>
+    <Intro/>
+    <About/>
+  </div>;
 };
 
 export default App;
