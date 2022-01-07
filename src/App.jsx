71e748af8711f0178bf1b72eb80a8c1ddd03542b
Ahmed J. Omer <ahmedjomer@gmail.com>
2022-01-06 20:33:08 -0800

    navbar implemented, maybe add new icon

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import Intro from '../src/components/introduction/Intro';
 import About from '../src/components/about/About';
 import EpisodeList from './components/episodeList/EpisodeList';
 import AboutW from '../src/components/about/AboutW';
+import Navbar from './components/navbar/Navbar';
 
 const App = () => {
   return <div>
+    <Navbar/>
     <Intro/>
     <About/>
     <AboutW/>
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import './navbar.css';
+import Coin from '../../imgs/coin.png';
+
+const Navbar = () => {
+    return (
+        <div className="topbar">
+            <div className="topLeft">
+                <a href='https://www.instagram.com/2coinpodcast/'><i className="iconList" class="fab fa-instagram" ></i></a>
+                <a href='https://twitter.com/2coinpodcast'><i className="iconList" class="fab fa-twitter"></i></a>
+                <a href='https://podcasts.apple.com/us/podcast/2coin-podcast/id1500805497'><i className="iconList" class="fas fa-podcast"></i></a>
+                <a href='https://open.spotify.com/show/4dqXBPhlMS4ooqo8T5nWQV?si=f50e1b4423444c59'><i className="iconList" class="fab fa-spotify"></i></a>
+            </div>
+            <div className="topCenter">
+                <ul className='topList'>
+                    <li className='topListItem'>HOME</li>
+                    <li className='topListItem'>ABOUT</li>
+                    <li className='topListItem'>PODCAST</li>
+                </ul>
+            </div>
+            <div className="topRight">
+                <img src={Coin} alt='coin icon' className='topImage'/>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
